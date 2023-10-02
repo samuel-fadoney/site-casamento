3ed47044ea6270506a59595e6e9d82172614acfa
samuel-fadoney <146787303+samuel-fadoney@users.noreply.github.com>
2023-10-02 20:38:02 -0300

    subi os arquivos

---
+++ script.js
@@ -0,0 +1,5 @@
+const proposalButton = document.getElementById("proposal-button");
+
+proposalButton.addEventListener("click", function () {
+    alert("Leticia, você aceita se casar comigo? 💍❤️");
+});
